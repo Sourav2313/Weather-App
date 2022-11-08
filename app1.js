a84fd1c4c410a0d9909e5e0629b7d94ca6a36787
Sourav2313 <sourav2313@gmail.com>
2022-11-08 13:03:17 +0530

    first commit

---
+++ app1.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+var weather = require('openweather-apis');
+const { TIMEOUT } = require('dns');
+
+//init app
+var app = express();
+
+//set the template engine
+app.set('view engine','ejs');
+
+// fetch the data from request
+app.use(bodyParser.urlencoded({extended:false}));
+
+//default pageload
+app.get('/',function(req,res){
+    res.render('home',{temp:null});
+});
+
+app.post('/',function(req,res){
+    weather.setCity(req.body.city);
+    weather.setAPPID('c802c73c229e2a5ecfb3bc9159b7565a');
+    weather.getAllWeather(function(err,temp){
+		console.log(temp);
+        res.render('home', {temp:temp});
+});
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log('server running at '+port);
+});
